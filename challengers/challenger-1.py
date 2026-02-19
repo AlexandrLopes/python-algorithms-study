f3438c4374da7452c2c5613abedacbbc999e61dd
Alexandre Lopes <alexandre.w.d.lopes@gmail.com>
2026-02-19 14:49:48 -0300

    First challenger complete

--- challengers/challenger-1.py
+++ challengers/challenger-1.py
@@ -1,3 +1,32 @@
+
+"""
+Write a for loop that goes through all instances in this list and prints the ID Environment only for servers whose State is equal to “running.”
+code: 
+
+aws_api_response = {
+    "Region": "us-east-1",
+    "Account": "1234567890",
+    "Instances": [
+        {
+            "Id": "i-0a1b2c", 
+            "State": "running", 
+            "Tags": {"Name": "Web-Server-01", "Environment": "Production"}
+        },
+        {
+            "Id": "i-0x9y8z", 
+            "State": "stopped" 
+            # Repare que este servidor não tem a chave "Tags"
+        },
+        {
+            "Id": "i-0p1q2r", 
+            "State": "running", 
+            "Tags": {"Name": "Database-Main"} 
+            # Repare que este tem "Tags", mas não tem a chave "Environment"
+        }
+    ]
+}
+"""
+
 aws_api_response = {
     "Region": "us-east-1",
     "Account": "1234567890",
@@ -8,13 +37,14 @@ aws_api_response = {
     ]
 }
 
-lista_de_servidores = aws_api_response["Instances"]
+server_list = aws_api_response["Instances"]
 
-for servidor in lista_de_servidores:
-    if servidor["State"] == "running":
+for server in server_list:
+    if server["State"] == "running":
+
         try:
-            ambiente = servidor["Tags"]["Environment"]
+            environment = server["Tags"]["Environment"]
         except KeyError:
-            ambiente = "Unknown"
+            environment = "Unknown"
             
-        print(f"Servidor {servidor['Id']} está no ambiente: {ambiente}")
+        print(f"Server {server['Id']} is in environment: {environment}")
